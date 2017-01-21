8090c7b9f8f4b8ce0cc46f63ef8ce48b41195c70
Jon Murray <jonmurray20@gmail.com>
2017-01-21 15:37:51 -0500

    Added camera follows playerscript, and death spikes script which also moves up to keep the player jumping to the top of the map.

--- Chameleon/Assets/Standard Assets/2D/Scripts/Restarter.cs
+++ Chameleon/Assets/Standard Assets/2D/Scripts/Restarter.cs
@@ -6,6 +6,12 @@ namespace UnityStandardAssets._2D
 {
     public class Restarter : MonoBehaviour
     {
+
+    	void Update(){
+
+    		transform.position += Vector3.up * Time.deltaTime;
+    	}
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.tag == "Player")
